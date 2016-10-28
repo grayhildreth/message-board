fdd21e58b77499d0acc494c86a6abf51e0cc9be4
Gray Hildreth <grayhildreth@gmail.com>
2016-10-28 16:14:42 -0700

    create answer model

---
+++ app/components/new-answer.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+     var params = {
+       response: this.get('response'),
+       author: this.get('author'),
+     };
+     this.set('addNewAnswer', false);
+     this.sendAction('saveAnswer', params);
+   }
+  }
+});
