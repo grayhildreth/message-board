2d1d628b808dc21a239de5ed16afbca9d1d606e2
Gray Hildreth <grayhildreth@gmail.com>
2016-10-28 09:56:14 -0700

    add new-question component

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        pregunta: this.get('pregunta') ? this.get('pregunta') : ""
+        additional: this.get('additional') ? this.get('additional') : ""
+        author: this.get('author') ? this.get('author') : ""
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
+});
