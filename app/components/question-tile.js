0c99ea16792b91a1222f3b029bd83292337dd62b
Gray Hildreth <grayhildreth@gmail.com>
2016-11-04 11:34:23 -0700

    add ember service, allow user to favorite question

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestion: Ember.inject.service(),
+
+  actions: {
+    addToFavorites(question) {
+      this.get('favoriteQuestion').add(question);
+    },
+
   answerCount: Ember.computed('question.answers', function() {
     var answerTotal = this.get('question.answers.length');
     return answerTotal;
     })
+  }
   });
